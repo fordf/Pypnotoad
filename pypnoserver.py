a0e298e54e724097279752949a49c394bd814f88
Ford Fowler <fordjfowler@gmail.com>
2018-03-27 18:21:34 -0700

    block game

--- pypnoserver.py
+++ pypnoserver.py
@@ -14,12 +14,13 @@ import websockets
 
 
 STEPSIZE = 30
+WIDTH, HEIGHT = 490, 490
 
 ACTIONS = {
-    'left': lambda pos: (pos[0], max(0, pos[1] - STEPSIZE)),
-    'right': lambda pos: (pos[0], min(300, pos[1] + STEPSIZE)),
-    'up': lambda pos: (max(0, pos[0] - STEPSIZE), pos[1]),
-    'down': lambda pos: (min(400, pos[0] + STEPSIZE), pos[1]),
+    'left': lambda pos: (max(0, pos[0] - STEPSIZE), pos[1]),
+    'right': lambda pos: (min(WIDTH, pos[0] + STEPSIZE), pos[1]),
+    'up': lambda pos: (pos[0], max(0, pos[1] - STEPSIZE)),
+    'down': lambda pos: (pos[0], min(HEIGHT, pos[1] + STEPSIZE)),
 }
 
 MESSAGE_ACTIONS = dict(zip(map(bin, range(20)), ACTIONS.keys()))
@@ -30,36 +31,47 @@ class Game(object):
     def __init__(self):
         self.players = {}
         self.loop = asyncio.get_event_loop()
+        self.player_id = 0
 
     async def connect(self, websocket, path):
-        self.players[websocket] = tuple(random.randint(0, 800) for _ in range(2))
+        self.player_id += 1
+        self.players[websocket] = {
+            'id': self.player_id,
+            'pos': [random.randint(0, WIDTH), random.randint(0, HEIGHT)]
+        }
         cors = [self.consumer(websocket), self.producer()]
         tasks = map(asyncio.ensure_future, cors)
-        done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-        for task in pending:
-            task.cancel()
-        del self.players[websocket]
-        print('player quit')
+        try:
+            done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+            for task in pending:
+                task.cancel()
+        finally:
+            del self.players[websocket]
+            print('player quit')
 
     async def consumer(self, websocket):
         while websocket.open:
             async for message in websocket:
                 print(message)
-                self.consume(websocket, message)
+                await self.consume(websocket, message)
+
 
     async def producer(self):
         while True:
-            await asyncio.wait([ws.send(self.get_state()) for ws in self.players])
-            await asyncio.sleep(1)
+            state = self.get_state()
+            await asyncio.wait([ws.send(state) for ws in self.players])
+            print(f'sent: {state}')
+            await asyncio.sleep(.1)
 
-    def consume(self, websocket, message):
-        pos = self.players[websocket]
+    async def consume(self, websocket, message):
+        pos = self.players[websocket]['pos']
         action = MESSAGE_ACTIONS[message]
         print(f'{id(websocket)}: {action}')
-        self.players[websocket] = ACTIONS[action](pos)
+        self.players[websocket]['pos'] = ACTIONS[action](pos)
+        await asyncio.sleep(.01)
 
     def get_state(self):
-        return '|'.join(map(str, self.players.values()))
+        return '|'.join(f'{v["id"]},' + ','.join(map(str, v['pos'])) for v in self.players.values())
 
 
 game = Game()
